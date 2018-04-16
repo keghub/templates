ddedf6d4abd1b54ce3c8964fa1ea236854f23e48
Renato Golia <renato.golia@studentum.se>
2018-04-16 15:16:44 +0200

    Added LambdaRunner template

---
+++ Templates/content/LambdaRunner/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LambdaRunner
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            /* TODO */
+            // 1) Add a project reference to the project containing your Lambda function
+            // 2) Uncomment one of the templates below according to your needs
+            // 3) Customize it so that it builds (input type, output type)
+            // 4) Make sure you are using the correct serializer
+            // 5) Make sure the port is not conflicting with other applications. If needed, customize it
+
+            /* Asynchronous function, accepting a string, returning a string, using default port (5000) and default serializer */
+            // await LambdaRunner.Create()
+            //                   .Receives<string>()
+            //                   .Returns<string>()
+            //                   .UsesAsyncFunction<Function>((function, input, context) => function.FunctionHandlerAsync(input, context))
+            //                   .Build()
+            //                   .RunAsync();
+
+            /* Synchronous function, accepting a string, returning a string, using default port (5000) and custom serializer */
+            // await LambdaRunner.Create()
+            //                   .UseSerializer(() => new MyCustomSerializer())
+            //                   .Receives<string>()
+            //                   .Returns<string>()
+            //                   .UsesFunction<Function>((function, input, context) => function.FunctionHandler(input, context))
+            //                   .Build()
+            //                   .RunAsync();
+
+            /* Asynchronous function, accepting a string with no result, using a custom port and default serializer */
+            // await LambdaRunner.Create()
+            //                   .UsePort(5001)
+            //                   .Receives<string>()
+            //                   .UsesAsyncFunctionWithNoResult<Function>((function, input, context) => function.FunctionHandlerAsync(input, context))
+            //                   .Build()
+            //                   .RunAsync();
+
+            /* Synchronous function, accepting a string with no result, using a custom port and custom serializer */
+            // await LambdaRunner.Create()
+            //                   .UsePort(5001)
+            //                   .UseSerializer(() => new MyCustomSerializer())
+            //                   .Receives<string>()
+            //                   .UsesFunctionWithNoResult<Function>((function, input, context) => function.FunctionHandler(input, context))
+            //                   .Build()
+            //                   .RunAsync();
+        }
+    }
+}

be92b36f1694cbc57a7b13c6efa4ab5fe2ae685d
Renato Golia <renato.golia@educations.com>
2019-04-08 16:15:04 +0200

    Added WebApiHost project

---
+++ Templates/content/WebApiHost/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiHost
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateWebHostBuilder(args).Build().RunAsync();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                          .UseContentRoot(AppDomain.CurrentDomain.BaseDirectory)
+                          .ConfigureAppConfiguration(ConfigureAppConfiguration)
+                          .ConfigureLogging(ConfigureLogging)
+                          .ConfigureKestrel(ConfigureKestrel)
+                          .UseStartup<Startup>();
+        }
+
+        private static void ConfigureLogging(WebHostBuilderContext context, ILoggingBuilder logging)
+        {
+            logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+
+            logging.AddLoggly(context.Configuration.GetSection("Loggly"));
+
+            if (context.HostingEnvironment.IsDevelopment())
+            {
+                logging.SetMinimumLevel(LogLevel.Debug);
+                logging.AddConsole();
+            }
+        }
+
+        private static void ConfigureAppConfiguration(WebHostBuilderContext context, IConfigurationBuilder builder)
+        {
+            var hostingEnvironment = context.HostingEnvironment;
+
+            // Creates a default sets of settings that can be overridden via file or environment variable
+            var settings = new Dictionary<string, string>
+            {
+                ["Loggly:ApplicationName"] = "WebApiHost",
+                ["Loggly:Environment"] = context.HostingEnvironment.EnvironmentName,
+
+                ["Loggly:ApiKey"] = "do-not-touch-this", // this is a fallback value. The real value should be provided via environment variables
+                ["JWT:SecretKey"] = "do-not-touch-this", // this is a fallback value. The real value should be provided via environment variables
+            };
+
+            builder.AddInMemoryCollection(settings);
+            builder.AddJsonFile("appsettings.json", true, true);
+            builder.AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true);
+            builder.AddEnvironmentVariables();
+        }
+
+        private static void ConfigureKestrel(WebHostBuilderContext context, KestrelServerOptions options)
+        {
+
+        }
+    }
+}
---
+++ Templates/content/WebApiHost/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EMG.Extensions.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApiHost
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(ConfigureMvc).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Adds support for ASP.NET Core health checks: https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-2.2
+            services.AddHealthChecks(); 
+
+            // Adds support for JWT authentication
+            services.AddJwtAuthentication(Configuration)
+                    
+                    // Extracts the authentication data from the form data of a POST request
+                    .AddFormUserExtractor() 
+                    
+                    // Matches incoming credentials with credentials fetched from the configuration values
+                    .AddBasicUserAuthenticator() 
+
+                    // If the delegate returns true, adds an authentication filter at global level that forbids anonymous requests
+                    .RequireAuthentication(() => HostingEnvironment.IsProduction()); 
+
+            void ConfigureMvc(MvcOptions options)
+            {
+                // Adds support for friendly error when an MVC action throws an uncaught exception
+                options.AddExceptionHandlerFilter();
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            // Requests matching "/health" are forwarded to the health check engine
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                AllowCachingResponses = false
+            });
+
+            // Uses JWT authentication flow
+            app.UseJwtAuthentication();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}

586a09fbb4651d767f6c4f3d3cce3143e58d7427
Renato Golia <golia.renato@gmail.com>
2019-04-09 12:36:40 +0200

    Template for API host projects (#3)
    * Added WebApiHost project
    * Added template configuration

---
+++ Templates/content/WebApiHost/Startup.cs
@@ -0,0 +1,119 @@
+﻿//#if (AddNybusBridge)
+using Amazon.SimpleNotificationService;
+using EMG.Common;
+//#endif
+using EMG.Extensions.AspNetCore;
+//#if (AddWcfDiscovery)
+using EMG.Wcf.Discovery;
+using EMG.Wcf.Discovery.Service;
+//#endif
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+//#if (AddNybus)
+using Nybus;
+//#endif
+
+namespace WebApiHost
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(ConfigureMvc).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Adds support for ASP.NET Core health checks: https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-2.2
+            services.AddHealthChecks(); 
+
+            // Adds support for JWT authentication
+            services.AddJwtAuthentication(Configuration)
+                    
+                    // Extracts the authentication data from the form data of a POST request
+                    .AddFormUserExtractor() 
+                    
+                    // Matches incoming credentials with credentials fetched from the configuration values
+                    .AddBasicUserAuthenticator() 
+
+                    // If the delegate returns true, adds an authentication filter at global level that forbids anonymous requests
+                    .RequireAuthentication(() => HostingEnvironment.IsProduction()); 
+//#if (AddNybus)
+            
+            // Configures Nybus to use RabbitMQ engine and fetch settings from the configuration values
+            services.AddNybus(nybus =>
+            {
+                nybus.UseConfiguration(Configuration);
+                
+                nybus.UseRabbitMqBusEngine(rabbitMq =>
+                {
+                    rabbitMq.UseConfiguration();
+                });
+            });
+
+            // Configures the NybusHostedService so that Nybus is started when the web application is started
+            services.AddHostedService<NybusHostedService>();
+//#endif
+//#if (AddWcfDiscovery)
+
+            // Configures the WCF Discovery from the current configuration
+            services.AddDiscovery(Configuration);
+            services.AddSingleton<IDiscoveryService, NetTcpDiscoveryService>();
+//#endif
+//#if (AddNybusBridge || ConfigureAWS)
+
+            // Configures AWS using the configuration values
+            services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+//#endif
+//#if (AddNybusBridge)
+            
+            // Registers the SNS client
+            services.AddAWSService<IAmazonSimpleNotificationService>();
+
+            services.AddSingleton<INybusBridge, SnsNybusBridge>();
+//#endif
+            
+            void ConfigureMvc(MvcOptions options)
+            {
+                // Adds support for friendly error when an MVC action throws an uncaught exception
+                options.AddExceptionHandlerFilter();
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            // Requests matching "/health" are forwarded to the health check engine
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                AllowCachingResponses = false
+            });
+
+            // Uses JWT authentication flow
+            app.UseJwtAuthentication();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
